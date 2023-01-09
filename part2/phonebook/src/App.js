a01051d5c5b21aec6d9f459754c32fdf2e9cbbaf
shivaramakrishnamarepalli <marepallishivaramakrishna@gmail.com>
2023-01-09 20:09:12 +0530

    phone book part 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,28 +1,35 @@
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
 import Filter from './components/Filter';
 import AddAcc from './components/AddAcc';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(()=>{
+    console.log("effect")
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => {
+      console.log("promise fulfilled")
+      setPersons(response.data)
+    })
+  },[])
 
   const [search, setSearch] = useState('');
   const [filter, setFilter] = useState([]);
-
-  
   const handleFilter = (event) => {
     const search = event.target.value;
-    const newFilter = persons.filter((person) =>
-    person.name.toLowerCase().startsWith(search.toLowerCase()) ||
-    person.number.startsWith(search)
-    );
+    const newFilter = persons.filter((person) => {
+      if (person.name && person.number) {
+        return person.name.toLowerCase().startsWith(search.toLowerCase()) ||
+               person.number.startsWith(search);
+      }
+    });
     setFilter(newFilter);
     setSearch(search);
   };
+  
     
   return (
     
